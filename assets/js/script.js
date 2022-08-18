46c69f0905f20130ecfb0b7c12498949e022a15e
Raenique27 <raeniquewalters@gmail.com>
2022-08-17 23:28:46 -0400

    added api to fetch weather for cities

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,4 +14,26 @@ var forcast5 = document.querySelector("#forcast-5");
 var forcastHeader = document.querySelector("#forcastHeader");
 var cities = [];
 
+var getCity = function (city) {
+    var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f58e39647952b54bd204a6b841a3ed8d`;
 
+    console.log(apiUrl);
+
+    fetch(apiUrl)
+        .then(function (response) {
+            if (response.ok) {
+                response.json()
+                    .then(function (data) {
+                        console.log(data);
+                    });
+            } else {
+                alert("Error cannot find city.");
+            }
+        })
+
+        .catch(function (error) {
+            alert("Cannot connect to server.");
+        });
+};
+
+getCity();
